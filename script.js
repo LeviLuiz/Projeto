e9dd899e8a0b5b56df4fb1024f91c22579bf6b32
Levi Luiz <leviluizmodesto@gmail.com>
2023-02-05 08:17:23 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function toogleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //if (html.classList.contains("light")) {
+  //html.classList.remove("light")
+  //} else {
+  //html.classList.add("light")
+  //}
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    alert("Modo Claro")
+    //se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.jfif")
+  } else {
+    //se tiver sem light mode, manter a imagem escura
+    img.setAttribute("src", "./assets/avatar.jfif")
+    alert("Modo Escuro")
+  }
+}
